e92ab79efd5130151c3f042430133641f76cd8c3
Ayush Gupta <Radhehanu143@gmail.com>
2025-12-14 05:09:34 +0530

    updated fonts for mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ["var(--font-theme)", "var(--font-sans)", "ui-sans-serif", "system-ui", "sans-serif"],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
